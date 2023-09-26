99807c8edcac59c9c9fe80f20517e4b0f2fcecc5
Adrián Santalla <adrian.santalla@gmail.com>
2023-09-26 17:27:37 +0200

    Modified the Stock API timeout config

--- src/co/develoop/mkm/api/builder/ApiClientBuilder.kt
+++ src/co/develoop/mkm/api/builder/ApiClientBuilder.kt
@@ -9,43 +9,43 @@ import retrofit2.converter.jaxb.JaxbConverterFactory
 import java.util.concurrent.TimeUnit
 
 class ApiClientBuilder(
-        appToken: String,
-        appSecret: String,
-        accessToken: String,
-        accessSecret: String
+    appToken: String,
+    appSecret: String,
+    accessToken: String,
+    accessSecret: String
 ) {
 
     companion object {
         private const val BASE_URL = "https://api.cardmarket.com/ws/v2.0/output.json/"
         private const val DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-        private const val TIMEOUT_SECONDS = 60L
+        private const val TIMEOUT_UNIT = 60L
     }
 
     private val oAuthHeaderProvider: OAuthHeaderProvider = OAuthHeaderProvider(
-            appToken = appToken,
-            appSecret = appSecret,
-            accessToken = accessToken,
-            accessSecret = accessSecret
+        appToken = appToken,
+        appSecret = appSecret,
+        accessToken = accessToken,
+        accessSecret = accessSecret
     )
 
     private val gson: Gson = GsonBuilder()
-            .setDateFormat(DATE_FORMAT)
-            .create()
+        .setDateFormat(DATE_FORMAT)
+        .create()
 
     private val okHttpClient: OkHttpClient = OkHttpClient()
-            .newBuilder()
-            .readTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
-            .connectTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
-            .addInterceptor(HeaderInterceptor(oAuthHeaderProvider))
-            .build()
+        .newBuilder()
+        .readTimeout(TIMEOUT_UNIT, TimeUnit.MINUTES)
+        .connectTimeout(TIMEOUT_UNIT, TimeUnit.MINUTES)
+        .addInterceptor(HeaderInterceptor(oAuthHeaderProvider))
+        .build()
 
     private val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addCallAdapterFactory(RxCallAdapterFactory.create())
-            .addConverterFactory(JaxbConverterFactory.create())
-            .addConverterFactory(GsonConverterFactory.create(gson))
-            .client(okHttpClient)
-            .build()
+        .baseUrl(BASE_URL)
+        .addCallAdapterFactory(RxCallAdapterFactory.create())
+        .addConverterFactory(JaxbConverterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .client(okHttpClient)
+        .build()
 
     fun <T> create(clazz: Class<T>): T {
         return retrofit.create(clazz)
